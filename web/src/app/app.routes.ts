1782dc26db0c2c55ce9799f77e6ab5370bed50d8
Robert Gill <envman@gmail.com>
2016-08-30 18:39:19 +0100

    ?

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,18 +2,11 @@ import { provideRouter, RouterConfig } from '@angular/router'
 
 import { CourseListComponent } from './course-list/course-list.component'
 import { DeveloperListComponent } from './developer-list/developer-list.component'
-// import { SurveyComponent } from './+survey/survey.component'
-// import { ImportComponent } from './import/import.component'
-// import { SurveyEditComponent } from './+survey-edit/survey-edit.component'
-// import { RuleEditComponent } from './+rule-edit/rule-edit.component'
-// import { HomeComponent } from './+home/home.component'
-// import { CanActivateViaAuthGuard } from './guards/canActivateViaAuth.guard'
 
 export const routes: RouterConfig = [
     { path: '', component: CourseListComponent },
     { path: 'courses', component: CourseListComponent },
     { path: 'developers', component: DeveloperListComponent },
-    // { path: 'survey', component: SurveyComponent },
     { path: '**', redirectTo: '/' }
 ]
 
