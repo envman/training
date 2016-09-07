e7a51e877d2128d3a8580ef28b15bd2558f6f784
Robert Gill <envman@gmail.com>
2016-09-07 18:30:41 +0100

    course detail routing

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { CourseListComponent } from './course-list/course-list.component'
 import { DeveloperListComponent } from './developer-list/developer-list.component'
 import { DeveloperComponent } from './developer/developer.component'
 import { AdminComponent } from './admin/admin.component'
+import { CourseDetailComponent } from './course-detail/course-detail.component'
 
 export const routes: RouterConfig = [
     { path: '', component: CourseListComponent },
     { path: 'courses', component: CourseListComponent },
+    { path: 'course/:id', component: CourseDetailComponent },
     { path: 'developers', component: DeveloperListComponent },
     { path: 'admin', component: AdminComponent },
     { path: 'developer/:id', component: DeveloperComponent },
--- web/src/app/course-detail/course-detail.component.ts
+++ web/src/app/course-detail/course-detail.component.ts
@@ -12,5 +12,4 @@ export class CourseDetailComponent {
 
   ngOnInit() {
   }
-
 }
